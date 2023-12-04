656d7764fac7b75712b6d3894e505faa1acbea06
gnemek20 <gnemek20@gmail.com>
2023-12-04 23:39:34 +0900

    찜 기능 추가

--- routes/users.js
+++ routes/users.js
@@ -23,8 +23,84 @@ sql.connect((error) => {
 // GET
 
 // POST
-router.get('/signin', (req, res) => {
+router.post('/signin', (req, res) => {
+  const user = {
+    id: req.body.user.id,
+    pwd: req.body.user.pwd
+  }
 
+  sql.query('select id, name from users where id=? and pwd=?', [user.id, user.pwd], (error, sqlResult) => {
+    if (sqlResult[0] == undefined) {
+      res.json({
+        status: false
+      });
+    }
+    else {
+      res.json({
+        status: true,
+        id: sqlResult[0].id,
+        name: sqlResult[0].name
+      });
+    }
+  });
+});
+
+router.post('/getProductSteamed', (req, res) => {
+  const inform = {
+    user_id: req.body.id,
+    product_id: req.body.product_id
+  }
+
+  sql.query('select * from productSteamedList where user_id="?" and product_id="?"', [inform.user_id, inform.product_id], (error, sqlResult) => {
+    if (error) {
+      console.log(error.message);
+      res.status(500).json();
+    }
+    else if (sqlResult[0] == undefined) {
+      res.json({
+        steamed: false
+      });
+    }
+    else {
+      res.json({
+        steamed: true
+      });
+    }
+  });
+});
+
+router.post('/setProductSteamed', (req, res) => {
+  const inform = {
+    user_id: req.body.id,
+    product_id: req.body.product_id
+  }
+
+  sql.query('insert into productSteamedList values("?", "?")', [inform.user_id, inform.product_id], (error) => {
+    if (!error) {
+      res.json();
+    }
+    else {
+      console.log(error.message);
+      res.status(500).json();
+    }
+  });
+});
+
+router.post('/deleteProductSteamed', (req, res) => {
+  const inform = {
+    user_id: req.body.id,
+    product_id: req.body.product_id
+  }
+
+  sql.query('delete from productSteamedList where user_id="?" and product_id="?"', [inform.user_id, inform.product_id], (error) => {
+    if (!error) {
+      res.json();
+    }
+    else {
+      console.log(error.message);
+      res.status(500).json();
+    }
+  });
 });
 
 module.exports = router;
